ee14eb99b16108c3591293ca57beba44753b2f06
𝑾𝒚𝒂𝒕𝒕·𝑳𝒗 <vlongen@vip.qq.com>
2023-05-27 00:03:32 +0800

    no message

---
+++ src/gulpfile.js
@@ -0,0 +1,128 @@
+import gulp from 'gulp'
+import debug from 'gulp-debug'
+import merge from 'gulp-merge-json'
+import jsoncombinearray from 'gulp-jsoncombine-array'
+import fs from 'fs'
+import path from 'path'
+import del from 'del'
+import zip from 'gulp-zip'
+import concat from 'gulp-concat'
+import rename from 'gulp-rename'
+import concatFolders from 'gulp-concat-folders'
+
+const generatorPhoneJSON = () => {
+  return gulp.src('dataset/phone/*.json')
+    .pipe(debug())
+    .pipe(merge({
+      fileName: 'phone-json.json',
+      concatArrays: true,
+      transform: (mergedJson) => {
+        return {
+          name:"防骚扰屏蔽-电话",
+          data: [...mergedJson.data,]
+        };
+      },
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+const generatorSMSJSON = () => {
+  return gulp.src('dataset/sms/*.json')
+    .pipe(debug())
+    .pipe(merge({
+      fileName: 'sms-json.json',
+      concatArrays: true,
+      transform: (mergedJson) => {
+        return {
+          name:"防骚扰屏蔽-短信",
+          data: [...mergedJson.data,]
+        };
+      },
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+const generatorPhoneArray = () => {
+  return gulp.src('dataset/phone/*.json')
+    .pipe(debug())
+    .pipe(jsoncombinearray("phone-array.json",function(dataArray) {
+      // do any work on data here
+      let data = []
+      dataArray.forEach((item, index)=>{
+        data.push(...item.data)
+      })
+      return new Buffer(JSON.stringify(data));
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+const generatorSMSArray = () => {
+  return gulp.src('dataset/sms/*.json')
+    .pipe(debug())
+    .pipe(jsoncombinearray("sms-array.json",function(dataArray) {
+      // do any work on data here
+      let data = []
+      dataArray.forEach((item, index)=>{
+        data.push(...item.data)
+      })
+      return new Buffer(JSON.stringify(data));
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+const generatorPhone = () => {
+  return gulp.src('dataset/phone/*.json')
+    .pipe(debug())
+    .pipe(jsoncombinearray("phone.json",function(dataArray) {
+      // do any work on data here
+      let data = []
+      dataArray.forEach((item, index)=>{
+        item.data.forEach((itemData, indexData)=>{
+          data.push(...Array.from(new Set([
+            itemData['prefix']
+          ])))
+        })
+      })
+      data = Array.from(new Set(data))
+      data = data.filter(item => item !== '');
+      return new Buffer(JSON.stringify(data));
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+const generatorSMS = () => {
+  return gulp.src('dataset/sms/*.json')
+    .pipe(debug())
+    .pipe(jsoncombinearray("sms.json",function(dataArray) {
+      // do any work on data here
+      let data = []
+      dataArray.forEach((item, index)=>{
+        item.data.forEach((itemData, indexData)=>{
+          data.push(...Array.from(new Set([
+            itemData['signature'],
+            itemData['binding'],
+            itemData['register'],
+            itemData['resetting'],
+          ])))
+        })
+      })
+      data = Array.from(new Set(data))
+      data = data.filter(item => item !== '');
+      return new Buffer(JSON.stringify(data));
+    }))
+    .pipe(gulp.dest('./dataset'))
+}
+
+const build = gulp.series(
+  generatorPhone,
+  generatorPhoneJSON,
+  generatorPhoneArray,
+  generatorSMS,
+  generatorSMSJSON,
+  generatorSMSArray
+)
+
+export {
+  generatorPhone,
+  generatorPhoneJSON,
+  generatorPhoneArray,
+  generatorSMS,
+  generatorSMSJSON,
+  generatorSMSArray,
+  build
+}
---
+++ src/utils/json.js
@@ -0,0 +1,34 @@
+export function saveJSON(data, filename) {
+  if (!data) {
+    alert("保存的数据为空");
+    return;
+  }
+  if (!filename) filename = "json.json";
+  if (typeof data === "object") {
+    data = JSON.stringify(data, undefined, 4);
+  }
+  var blob = new Blob([data], { type: "text/json" }),
+    e = document.createEvent("MouseEvents"),
+    a = document.createElement("a");
+  a.download = filename;
+  a.href = window.URL.createObjectURL(blob);
+  a.dataset.downloadurl = ["text/json", a.download, a.href].join(":");
+  e.initMouseEvent(
+    "click",
+    true,
+    false,
+    window,
+    0,
+    0,
+    0,
+    0,
+    0,
+    false,
+    false,
+    false,
+    false,
+    0,
+    null
+  );
+  a.dispatchEvent(e);
+}
